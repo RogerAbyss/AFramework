e947c25f6617278a26febaaada36841cd2b73ed8
abyss <roger_ren@qq.com>
2019-10-21 22:41:02 +0800

    no message

--- AFramework/Sources/Utils/DefaultsUtil.swift
+++ AFramework/Sources/Utils/DefaultsUtil.swift
@@ -38,7 +38,7 @@ public class DefaultsUtil {
                 #if DEBUG
                     return "http://47.74.144.109/drt"
                 #else
-                    return "http://korea.togcm.com"
+                    return "http://korea.togcm.com/drt"
                 #endif
             } else {
                 return PlistUtil.getStringValue("APP_HOST")
